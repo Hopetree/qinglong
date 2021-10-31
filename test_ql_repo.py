a92c5fd8109d29a92780c2d739f392e03a0891fd
hopetree <zlwork2014@163.com>
2021-10-31 16:55:25 +0800

    test ql repo

--- test_ql_repo.py
+++ test_ql_repo.py
@@ -3,8 +3,8 @@
 # @Author: Hopetree <https://github.com/Hopetree>
 # @Time  : 2021/10/31
 # @Desc  :
-# cron=10 7,18 * * * test_ql_repo.py
-# new Env('repo 测试');
+# cron=10 9,18 * * *
+# new Env('repo 测试1');
 
 import os
 
